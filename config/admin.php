dc75db9565d035c304895be2acc00223c564b755
Adrìa Martínez <amartinezp@fp.insjoaquimmir.cat>
2023-10-18 19:07:53 +0200

    Codi base del projecte

---
+++ config/admin.php
@@ -0,0 +1,8 @@
+<?php
+
+
+return [
+   'name'      => env('ADMIN_NAME', 'admin'),
+   'email'     => env('ADMIN_EMAIL', '2daw.equip12@fp.insjoaquimmir.cat'),
+   'password'  => env('ADMIN_PASSWORD', 'kS4l0S|275&!'),
+];
