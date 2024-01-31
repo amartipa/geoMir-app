0624f8b89bc56525e3e0f22e8413a4c064251423
Adrià Martínez <amartinezp@fp.insjoaquimmir.cat>
2024-01-31 19:45:33 +0100

    Trabajando postman

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\Api\FileController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -13,6 +14,8 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+Route::apiResource('files', FileController::class);
+Route::post('files/{file}', [FileController::class, 'update_workaround']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
